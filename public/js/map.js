6362164b1edc37b4eb250b8ebca840fd4d6bdb4a
= <ps7960040@gmail.com>
2025-03-30 18:00:40 +0530

    Add Project Files

---
+++ public/js/map.js
@@ -0,0 +1,18 @@
+
+maptilersdk.config.apiKey = 'MPnblNlzoaYurCcY1Q6H';
+const map = new maptilersdk.Map({
+  container: 'map', // container's id or the HTML element to render the map
+  style: maptilersdk.MapStyle.STREETS,
+  center: [77.2088,28.6139],   // starting postion [lng ,lat]
+  zoom:9,
+
+
+
+});
+
+
+
+
+
+
+
